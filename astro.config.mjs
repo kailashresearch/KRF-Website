9c1a28947410fb9d0d893036835a7e301b2a4a3f
Adwaith Rao <adwaithraop@gmail.com>
2026-03-05 17:32:58 +0530

    Update the website domain name and prefix URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kailashresearch.github.io",
+  site: "https://kailashresearch.org/",
   base: "/KRF-Website",
   vite: {
     plugins: [tailwindcss()]
